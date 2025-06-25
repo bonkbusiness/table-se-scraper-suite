1c7eb0f745da5377a1525473b1a3818d02ba3e4f
bonkbusiness <mail@hallokaj.com>
2025-06-25 22:47:42 +0200

    Create exclusions.py

---
+++ exclusions.py
@@ -0,0 +1,28 @@
+EXCLUDED_CATEGORIES = [
+    "Hyra container",
+    "Förrådscontainrar",
+    "Kylcontainrar",
+    "Fryscontainrar",
+    "Kök & disk",
+    "Toalettbodar",
+    "Kontorsbodar",
+    "Eventcontainrar",
+    "Specialcontainrar",
+    "Containertillbehör",
+    "Köpa container",
+    "Begagnade containrar",
+    "Self storage",
+    "Flytt & förvaringsservice",
+    "Transporter"
+    "Teknik"
+    "Tält",
+]
+
+# Add product names, SKUs, or URL substrings to skip. Example:
+# "Specifik produktnamn", "12345", "/produkt/url-del/"
+EXCLUDED_PRODUCTS = [
+    # Example entries:
+    # "Exakt produktnamn",
+    # "12345",
+    # "/produkt/url-del/",
+]
