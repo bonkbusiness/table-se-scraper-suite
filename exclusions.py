ddbf69170d322e21ed3605c94302c7599f8dc0a9
bonkbusiness <mail@hallokaj.com>
2025-06-26 14:31:52 +0200

    Update exclusions.py

--- exclusions.py
+++ exclusions.py
@@ -1,34 +1,7 @@
-EXCLUDED_CATEGORIES = [
-    "Hyra container",
-    "Förrådscontainrar",
-    "Kylcontainrar",
-    "Fryscontainrar",
-    "Kök & disk",
-    "Toalettbodar",
-    "Kontorsbodar",
-    "Eventcontainrar",
-    "Specialcontainrar",
-    "Containertillbehör",
-    "Köpa container",
-    "Begagnade containrar",
-    "Self storage",
-    "Flytt & förvaringsservice",
-    "Transporter"
-    "Teknik"
-    "Tält",
-]
-
-# Add product names, SKUs, or URL substrings to skip. Example:
-# "Specifik produktnamn", "12345", "/produkt/url-del/"
-EXCLUDED_PRODUCTS = [
-    # Example entries:
-    # "Exakt produktnamn",
-    # "12345",
-    # "/produkt/url-del/",
-    "https://www.table.se/produkter/container/self-storage/transporter/"
-    "https://www.table.se/produkter/container/self-storage/"
-    "https://www.table.se/produkter/container/kopa-container/"
-    "https://www.table.se/produkter/container/hyra-container/"
-    "https://www.table.se/produkter/teknik/"
-    "https://www.table.se/produkter/talt/"
+EXCLUDED_URL_PREFIXES = [
+    # If any URL starts with these, that category and all subcategories/products will be excluded
+    "https://www.table.se/produkter/container/",
+    "https://www.table.se/produkter/teknik/",
+    "https://www.table.se/produkter/talt/",
+    # Add more URL prefixes as needed
 ]
