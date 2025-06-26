0b9a7137ea6fb4dee1051d8c3a61cfef2f88991e
bonkbusiness <mail@hallokaj.com>
2025-06-26 19:52:22 +0200

    Update exclusions.py

--- exclusions.py
+++ exclusions.py
@@ -1,7 +1,10 @@
 EXCLUDED_URL_PREFIXES = [
-    # If any URL starts with these, that category and all subcategories/products will be excluded
     "https://www.table.se/produkter/container/",
     "https://www.table.se/produkter/teknik/",
     "https://www.table.se/produkter/talt/",
-    # Add more URL prefixes as needed
+    # Add more as needed
 ]
+
+def is_excluded(url: str) -> bool:
+    """Return True if the URL should be excluded based on prefix list."""
+    return any(url.startswith(prefix) for prefix in EXCLUDED_URL_PREFIXES)
